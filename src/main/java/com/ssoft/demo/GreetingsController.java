9117e372075a9b35f1b25696bc1dba7ab7ca648a
sreejutr <sreejutr@gmail.com>
2021-02-01 00:00:30 -0500

    Update GreetingsController.java

--- src/main/java/com/ssoft/demo/GreetingsController.java
+++ src/main/java/com/ssoft/demo/GreetingsController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class GreetingsController {
 
+    
     @RequestMapping("/")
     public @ResponseBody
     String greeting() {
